dbc2e99d6172db200929551632c0a0e4dfaabd31
Rafael Murad <rflmurad@gmail.com>
2022-02-08 17:09:52 -0300

    refactor: addRawMaterial now persisting data in db

--- src/controllers/rawMaterials/addRawMaterialsController.js
+++ src/controllers/rawMaterials/addRawMaterialsController.js
@@ -1,11 +1,28 @@
+const { insertOne, findOne, updateOne } = require("../../helpers/db");
+
 async function addRawMaterials(req, res) {
   try {
     const { name, quantity, user } = req.body;
-    return res.json({
+    const rawMaterialExists = await findOne("rawMaterials", { name });
+
+    if (rawMaterialExists) {
+      const rawMaterialExists = await updateOne("rawMaterials", {
+        query: { name },
+        update: {
+          $inc: { quantity: quantity },
+          $set: { user: user },
+        },
+      });
+      return res.status(200).json(rawMaterialExists);
+    }
+
+    const rawMaterial = await insertOne("rawMaterials", {
       name,
       quantity,
       user,
     });
+
+    return res.status(201).json(rawMaterial);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
