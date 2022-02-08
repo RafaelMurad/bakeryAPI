fbb46df721384abf76b788e2d3ce659fcaa2bfbc
Rafael Murad <rflmurad@gmail.com>
2022-02-08 20:29:00 -0300

    refactor: improved error handling

--- src/controllers/rawMaterials/updateRawMaterialsController.js
+++ src/controllers/rawMaterials/updateRawMaterialsController.js
@@ -2,35 +2,62 @@ const { findOne, updateOne, insertOne } = require("../../helpers/db");
 const ObjectId = require("mongodb").ObjectId;
 
 async function updateRawMaterials(req, res) {
-  const id = req.params.id;
-  const { quantity, user } = req.body;
+  try {
+    const id = req.params.id;
+    const { quantity, user } = req.body;
 
-  const rawMaterials = await findOne("rawMaterials", { _id: ObjectId(id) });
-  if (!rawMaterials) {
-    return res.status(404).json({
-      error: "Raw Material not found",
-    });
-  }
-  if (rawMaterials.quantity >= quantity) {
-    await updateOne("rawMaterials", {
-      query: { _id: ObjectId(id) },
-      update: { $inc: { quantity: -quantity } },
-    });
+    const rawMaterials = await findOne("rawMaterials", { _id: ObjectId(id) });
+    if (!rawMaterials) {
+      return res.status(404).json({
+        error: "Raw Material not found",
+      });
+    }
 
-    await insertOne("transactions", {
-      materialId: new ObjectId(id),
-      name: rawMaterials.name,
-      quantity: quantity,
-      user: user,
-      createdAt: new Date(),
-    });
+    if (rawMaterials.quantity >= quantity) {
+      try {
+        await updateOne("rawMaterials", {
+          query: { _id: ObjectId(id) },
+          update: { $inc: { quantity: -quantity } },
+        });
 
-    return res.status(200).json({
-      message: "Raw Material updated successfully",
-    });
-  } else {
-    return res.status(409).json({
-      error: "Raw Material quantity is not enough",
+        try {
+          await insertOne("transactions", {
+            materialId: new ObjectId(id),
+            name: rawMaterials.name,
+            quantity: quantity,
+            user: user,
+            createdAt: new Date(),
+          });
+        } catch (err) {
+          return res.status(206).json({
+            withdraw: true,
+            transaction: false,
+            error: "Error while updating transaction",
+          });
+        }
+      } catch (error) {
+        return res.status(500).json({
+          withdraw: false,
+          transaction: false,
+          error: "Error while updating raw material",
+        });
+      }
+
+      const updatedRawMaterials = await findOne("rawMaterials", {
+        _id: ObjectId(id),
+      });
+
+      return res.status(200).json({
+        ...updatedRawMaterials,
+      });
+    } else {
+      return res.status(409).json({
+        error: "Raw Material quantity is not enough",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
     });
   }
 }
